a0f4975c9c3e5a2e7db6fa93b01d0db2df5cf569
okpocandy <opcandy@naver.com>
2025-06-30 11:09:18 +0900

    RPC로 스테미나 동기화, 캐릭터 모델 변경

--- Assets/02.Scripts/Player/PlayerAttackAbility.cs
+++ Assets/02.Scripts/Player/PlayerAttackAbility.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class PlayerAttackAbility : PlayerAbility
@@ -17,6 +18,12 @@ public class PlayerAttackAbility : PlayerAbility
         _animator = GetComponent<Animator>();
     }
 
+    // - 위치/회전처럼 상시로 확인이 필요한 데이터 동기화: IPunObservable(OnPhotonSerializeView)
+    // - '트리거/공격/피격' 처럼 간헐적으로 특정한 이벤트가 발생했을때의 변화된 데이터 동기화 : RPC
+    // RPC : Remote Procedure Call
+    //       ㄴ 물리적으로 떨어져 있는 다른 디바이스의 함수를 호출하는 기능
+    //       ㄴ RPC 함수를 호출하면 네트워클르 통해 다른 사용자의 스크립트에서 해당 함수가 호출된다.
+
     private void Update()
     {
         if(!_photonView.IsMine)
@@ -31,10 +38,17 @@ public class PlayerAttackAbility : PlayerAbility
             return;
         }
 
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && _owner.TryConsumeStamina(_owner.Stat.StaminaAttackCost))
         {
             _attackTimer = 0f;
-            _animator.SetTrigger($"attack{Random.Range(1, 4)}");
+            //PlayAttackAnimation(Random.Range(1,4));
+            _photonView.RPC(nameof(PlayAttackAnimation), RpcTarget.All, Random.Range(1,4));
         }
+    }
+
+    [PunRPC]
+    private void PlayAttackAnimation(int randomNumber)
+    {
+        _animator.SetTrigger($"attack{randomNumber}");
     }
 }
--- Assets/02.Scripts/Player/PlayerStat.cs
+++ Assets/02.Scripts/Player/PlayerStat.cs
@@ -10,6 +10,7 @@ public class PlayerStat
     public float AttackSpeed = 1f;  // 초당 1번 공격할 수 있다.
 
     // 스테미너
+    [Header("스테미너")]
     public float MaxStamina = 100f;
     public float StaminaRecoverySpeed = 20f;
     public float StaminaRunCost = 10f;
@@ -17,5 +18,6 @@ public class PlayerStat
     public float StaminaAttackCost = 20f;
 
     // 체력
+    [Header("체력")]
     public float MaxHealth = 100f;
 }
